87dcaafe83151b5f6fe76242c2cdd4937ce07d01
callmedeee <daviasmickle@gmail.com>
2022-11-11 21:06:32 -0500

    exercise 5 php implementation

--- world.php
+++ world.php
@@ -4,30 +4,75 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+$context = htmlentities($_GET['context'],ENT_QUOTES,'utf-8');
+if($context != 'cities'){
+	$country = htmlentities($_GET['country'],ENT_QUOTES,'utf-8');
+	$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$country = htmlentities($_GET['country'],ENT_QUOTES,'utf-8');
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo "<table>
+	<br>
+	
+	<br>
 
-echo "<table>
-<tr>
-<th>Name</th>
-<th>Continent</th>
-<th>Independence Year</th>
-<th>Head of State</th>
-</tr>";
+	<tr>
+
+	<th>Name</th>
+
+	<th>Continent</th>
+
+	<th>Independence Year</th>
+
+	<th>Head of State</th>
+
+	</tr>";
+
+	foreach ($results as $row){
+		echo "<tr>";
+        	echo"<td>"; echo$row['name']; echo"</td>";
+       	echo"<td>"; echo$row['continent']; echo"</td>";
+       	echo"<td>"; echo$row['independence_year']; echo"</td>";
+        	echo"<td>"; echo$row['head_of_state']; echo"</td>";
+        	echo"</tr>";
+     }
+}else{
+    $country = htmlentities($_GET['country'],ENT_QUOTES,'utf-8');
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries INNER JOIN cities ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table class='tbl2'>
+
+    <br>
+
+    <br> 
+
+    <tr>
+
+    <th>Name</th>
+
+    <th>District</th>
+
+    <th>Population</th>
+
+    </tr>";
+
+    foreach ($results as $row){
+        echo "<tr>";
+        echo"<td>"; echo$row['name']; echo"</td>";
+        echo"<td>"; echo$row['district']; echo"</td>";
+        echo"<td>"; echo$row['population']; echo"</td>";
+        echo"</tr>";
+    }
+
+
+
+}
+
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
   
-<tr>
-                    <td><?= $row['name']; ?></td>
-                    <td><?= $row['continent']; ?></td>
-                    <td><?= $row['independence_year']; ?></td>
-                    <td><?= $row['head_of_state']; ?></td>
- </tr>
-<?php endforeach; ?>
-</ul>
+
